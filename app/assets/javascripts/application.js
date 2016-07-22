442ff07de4e90d86ec5aa0fd43f4e5ef865eaaa4
jlee257 <jlee257@berkeley.edu>
2016-07-22 02:17:02 -0700

    navbar and scroll event triggers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,50 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap-sprockets
+//= require waypoints
 //= require_tree .
+
+
+$(document).ready(function() {
+  $("#section1").waypoint(function(direction) {
+    if (direction == "up") {
+      $("ul.nav li").removeClass("active");
+      $("ul.nav li.menu-home").addClass("active");
+    } else {
+      $("ul.nav li").removeClass("active");
+      $("ul.nav li.menu-section1").addClass("active");
+    }
+  }, {offset: "50%"});
+
+  // jQuery("a").click(function(event){
+  //   //check if it has a hash (i.e. if it's an anchor link)
+  //   if(this.hash){
+  //     var hash = this.hash.substr(1);
+  //     var $toElement = jQuery("[id="+hash+"]");
+  //     var toPosition = $toElement.position().top;
+  //     //scroll to element
+  //     $("body,html").animate({
+  //       scrollTop : toPosition - 70
+  //     } ,1000)
+  //   }
+  // });
+  $("#a-menu-home").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $("#home").offset().top
+    }, 300);
+  });
+
+  $("#a-menu-section1").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $("#section1").offset().top
+    }, 300);
+  });
+});
+
+
+
